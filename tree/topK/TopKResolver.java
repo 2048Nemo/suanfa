2435461ab0d555c190af6ca865947376e2e25eb4
NemoLexist <2039152071@qq.com>
2024-10-07 20:07:35 +0800

    fist commit

---
+++ tree/topK/TopKResolver.java
@@ -0,0 +1,15 @@
+package tree.topK;
+
+/**
+ * @BelongsProject: suanfa
+ * @BelongsPackage: tree.topK
+ * @Author: nemo2048
+ * @CreateTime: 2024-10-07  19:16
+ * @Description: TODO
+ * @Version: 1.0
+ */
+public class TopKResolver {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
